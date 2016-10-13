624a40443bc89bdbe525e8ec71956e2c840a48e9
zachstecher <malleablemedia@gmail.com>
2016-10-12 20:43:22 -0400

    Update midterm2.py

--- Stecher-midterm/midterm2.py
+++ Stecher-midterm/midterm2.py
@@ -4,11 +4,13 @@ N = 1000
 n = 0
 it = 0
 
+#While the difference is more than 1, we haven't yet converged.
 while((N - n) > 1):
   it += 1
   n = N
   N = (8/(0.05**2))*math.log((4*((2*N)**10 + 1)/0.05))
   
+  #Just in case something goes wrong, to prevent an infinite loop.
   if(it == 20):
     break
 
