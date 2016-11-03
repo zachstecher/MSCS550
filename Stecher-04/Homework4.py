db665e5c61223bc5da5b1534e871484894411d3e
zachstecher <malleablemedia@gmail.com>
2016-11-02 23:49:40 -0400

    Update Homework4.py

--- Stecher-04/Homework4.py
+++ Stecher-04/Homework4.py
@@ -13,10 +13,10 @@ X, y, ytrue = hw4genData.genDataSet(1000)
 
 X = X.reshape((len(X), 1))
 
-bestk=[]
+bestk=[] #Holds every k-neighbors value, we will sort by score later to get the best 3 first
 kc=0
-bestklist=[]
-for i in range(0, 100):
+bestklist=[] #Holds the best 3 k-neighbors from each repetition of the experiment for problem 2
+for i in range(0, 100): #Performs the experiment 100 times for problem 2
   for n_neighbors in range(1,900,2):
     kf = KFold(n_splits=10)
     kscore=[]
@@ -40,17 +40,21 @@ for i in range(0, 100):
     #print bestk[kc]
     kc+=1
 
-  idx = sorted(range(len(bestk)), key=bestk.__getitem__)
+  idx = sorted(range(len(bestk)), key=bestk.__getitem__) #Sort the k-neighbors values
 #  print(idx[-1]*2)
 #  print(idx[-2]*2)
 #  print(idx[-3]*2)
+
+  #Add the best 3 values to our overall list for problem 2
   bestklist.append(idx[-1]*2)
   bestklist.append(idx[-2]*2)
   bestklist.append(idx[-3]*2)
-  bestk[:] = []
+  bestk[:] = [] #Clear bestk so we can run the experiment again on a clean array
   
-print kscore[0]
-print bestklist
+print kscore[0] #Print the Eout
+print bestklist #Print the list of best k-neighbors values from each experiment
+
+#Plot a histogram of each 
 plt.hist(bestklist)
 plt.title("KNeighbor Results")
 plt.xlabel("Number of Neighbors")
